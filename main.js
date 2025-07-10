4c2dba613453f94d9a9d718a41e8461f3b606048
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2025-07-09 20:32:28 -0400

    feat: able to edit exchange rates for fill rows

--- main.js
+++ main.js
@@ -3,45 +3,33 @@
 const slider_groups = [
     {
         name: "fruit",
-        cho_rate: 15,
-        pro_rate: 0,
-        fat_rate: 0
+        rates: [15, 0, 0]
     },
     {
         name: "veg",
-        cho_rate: 5,
-        pro_rate: 2,
-        fat_rate: 0
+        rates: [5, 2, 0]
     },
     {
         name: "milk",
-        cho_rate: 12,
-        pro_rate: 8,
-        fat_rate: 3
+        rates: [12, 8, 3]
     }
 ];
 
 const fill_groups = [
     {
         name: "starch",
-        goal_name: "cho",
-        cho_rate: 15,
-        pro_rate: 3,
-        fat_rate: 1
+        rates: [15, 3, 1],
+        goal_index: 0
     },
     {
         name: "pro",
-        goal_name: "pro",
-        cho_rate: 0,
-        pro_rate: 7,
-        fat_rate: 4
+        rates: [0, 7, 4],
+        goal_index: 1
     },
     {
         name: "fat",
-        goal_name: "fat",
-        cho_rate: 0,
-        pro_rate: 0,
-        fat_rate: 5
+        rates: [0, 0, 5],
+        goal_index: 2
     }
 ];
 
@@ -51,111 +39,229 @@ const table = document.querySelector(".js-table");
 const slider_group_template = document.querySelector(".js-slider-group-template");
 const fill_group_template = document.querySelector(".js-fill-group-template");
 const last_row = document.querySelector(".js-last-row");
-const calculated_cho = document.querySelector(".js-calculated-cho");
-const calculated_pro = document.querySelector(".js-calculated-pro");
-const calculated_fat = document.querySelector(".js-calculated-fat");
+const goal_nutrients = [
+    document.querySelector(".js-goal-cho"),
+    document.querySelector(".js-goal-pro"),
+    document.querySelector(".js-goal-fat")
+];
+const calculated_nutrients = [
+    document.querySelector(".js-calculated-cho"),
+    document.querySelector(".js-calculated-pro"),
+    document.querySelector(".js-calculated-fat")
+];
 
 /* Slider Groups */
 
 for (const group of slider_groups) {
+    // Clone template
     const new_row = slider_group_template.content.cloneNode(true).firstElementChild;
 
+    // Query-select elements
     const slider = new_row.querySelector(".js-slider");
     const servings = new_row.querySelector(".js-servings");
     const servings_label = new_row.querySelector(".js-servings-label");
     const name_label = new_row.querySelector(".js-name-label");
-    const cho = new_row.querySelector(".js-cho");
-    const cho_cell = new_row.querySelector(".js-cho-cell");
-    const cho_rate = new_row.querySelector(".js-cho-rate");
-    const cho_rate_cell = new_row.querySelector(".js-cho-rate-cell");
-    const pro = new_row.querySelector(".js-pro");
-    const pro_cell = new_row.querySelector(".js-pro-cell");
-    const pro_rate = new_row.querySelector(".js-pro-rate");
-    const pro_rate_cell = new_row.querySelector(".js-pro-rate-cell");
-    const fat = new_row.querySelector(".js-fat");
-    const fat_cell = new_row.querySelector(".js-fat-cell");
-    const fat_rate = new_row.querySelector(".js-fat-rate");
-    const fat_rate_cell = new_row.querySelector(".js-fat-rate-cell");
+    const nutrients = [
+        new_row.querySelector(".js-cho"),
+        new_row.querySelector(".js-pro"),
+        new_row.querySelector(".js-fat")
+    ];
+    const rates = [
+        new_row.querySelector(".js-cho-rate"),
+        new_row.querySelector(".js-pro-rate"),
+        new_row.querySelector(".js-fat-rate")
+    ];
+    const nutrient_cells = [
+        new_row.querySelector(".js-cho-cell"),
+        new_row.querySelector(".js-pro-cell"),
+        new_row.querySelector(".js-fat-cell")
+    ];
+    const rate_cells = [
+        new_row.querySelector(".js-cho-rate-cell"),
+        new_row.querySelector(".js-pro-rate-cell"),
+        new_row.querySelector(".js-fat-rate-cell")
+    ];
 
+    // Set 'id' and 'for' attributes for inputs and labels
     slider.setAttribute("id", `html-${group.name}-slider`);
     servings_label.setAttribute("for", `html-${group.name}-slider`);
     name_label.setAttribute("for", `html-${group.name}-slider`);
-    name_label.innerText = group.name.toUpperCase();
-    cho_rate.value = group.cho_rate;
-    pro_rate.value = group.pro_rate;
-    fat_rate.value = group.fat_rate;
 
-    function updateChoProFatDisplays() {
-        cho.value = `${group.cho_rate * servings.value} g`;
-        pro.value = `${group.pro_rate * servings.value} g`;
-        fat.value = `${group.fat_rate * servings.value} g`;
+    // Set nutrient name and exchange rates
+    name_label.innerText = group.name.toUpperCase();
+    for (let i = 0; i < rates.length; ++i) {
+        rates[i].value = group.rates[i];
     }
 
+    // Attach event listeners
     slider.addEventListener("input", () => {
         servings.value = slider.value;
-        updateChoProFatDisplays();
+        updateNutrientDisplays();
     });
 
-    new_row.addEventListener("mouseover", () => {
-        cho_cell.setAttribute("colspan", 1);
-        pro_cell.setAttribute("colspan", 1);
-        fat_cell.setAttribute("colspan", 1);
-        cho_rate_cell.removeAttribute("hidden");
-        pro_rate_cell.removeAttribute("hidden");
-        fat_rate_cell.removeAttribute("hidden");
-    });
+    for (let i = 0; i < nutrient_cells.length; ++i) {
+        nutrient_cells[i].addEventListener("mouseenter", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                showRateCells();
+            }
+        });
 
-    new_row.addEventListener("mouseleave", () => {
-        cho_cell.setAttribute("colspan", 2);
-        pro_cell.setAttribute("colspan", 2);
-        fat_cell.setAttribute("colspan", 2);
-        cho_rate_cell.setAttribute("hidden", "hidden");
-        pro_rate_cell.setAttribute("hidden", "hidden");
-        fat_rate_cell.setAttribute("hidden", "hidden");
-    });
+        rate_cells[i].addEventListener("mouseenter", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                showRateCells();
+            }
+        });
 
-    cho_rate.addEventListener("input", ({target: rate}) => {
-        group.cho_rate = rate.value;
-        updateChoProFatDisplays();
-    });
+        nutrient_cells[i].addEventListener("mouseleave", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                hideRateCells();
+            }
+        });
 
-    pro_rate.addEventListener("input", ({target: rate}) => {
-        group.pro_rate = rate.value;
-        updateChoProFatDisplays();
-    });
+        rate_cells[i].addEventListener("mouseleave", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                hideRateCells();
+            }
+        });
+    }
 
-    fat_rate.addEventListener("input", ({target: rate}) => {
-        group.fat_rate = rate.value;
-        updateChoProFatDisplays();
-    });
+    for (let i = 0; i < rates.length; ++i) {
+        rates[i].addEventListener("input", ({target}) => {
+            group.rates[i] = target.value;
+            updateNutrientDisplays();
+        });
+    }
 
+    // Insert new row to table
     last_row.parentNode.insertBefore(new_row, last_row);
 
+    // Save food group's servings count for future calculations
     group.servings = servings;
+
+    // Define hoisted utility functions
+    function updateNutrientDisplays() {
+        for (let i = 0; i < nutrients.length; ++i) {
+            nutrients[i].value = `${rates[i].value * servings.value} g`;
+        }
+    }
+
+    function showRateCells() {
+        for (const cell of rate_cells) {
+            cell.style.setProperty("opacity", "1");
+        }
+    }
+
+    function hideRateCells() {
+        for (const cell of rate_cells) {
+            cell.style.removeProperty("opacity");
+        }
+    }
+
+    function isHoverableElement(element) {
+        return nutrient_cells.includes(element) || rate_cells.includes(element);
+    }
 }
 
 /* Fill Groups */
 
 for (const group of fill_groups) {
+    // Clone template
     const new_row = fill_group_template.content.cloneNode(true).firstElementChild;
 
+    // Query-select elements
     const servings = new_row.querySelector(".js-servings");
     const name_cell = new_row.querySelector(".js-name-cell");
-    const cho = new_row.querySelector(".js-cho");
-    const pro = new_row.querySelector(".js-pro");
-    const fat = new_row.querySelector(".js-fat");
-    const goal = document.querySelector(`.js-goal-${group.goal_name}`);
+    const nutrients = [
+        new_row.querySelector(".js-cho"),
+        new_row.querySelector(".js-pro"),
+        new_row.querySelector(".js-fat")
+    ];
+    const rates = [
+        new_row.querySelector(".js-cho-rate"),
+        new_row.querySelector(".js-pro-rate"),
+        new_row.querySelector(".js-fat-rate")
+    ];
+    const nutrient_cells = [
+        new_row.querySelector(".js-cho-cell"),
+        new_row.querySelector(".js-pro-cell"),
+        new_row.querySelector(".js-fat-cell")
+    ];
+    const rate_cells = [
+        new_row.querySelector(".js-cho-rate-cell"),
+        new_row.querySelector(".js-pro-rate-cell"),
+        new_row.querySelector(".js-fat-rate-cell")
+    ];
 
+    // Set nutrient name and exchange rates
     name_cell.innerText = group.name.toUpperCase();
+    for (let i = 0; i < rates.length; ++i) {
+        rates[i].value = group.rates[i];
+    }
 
+    // Attach event listeners
+    for (let i = 0; i < nutrient_cells.length; ++i) {
+        nutrient_cells[i].addEventListener("mouseenter", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                showRateCells();
+            }
+        });
+
+        rate_cells[i].addEventListener("mouseenter", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                showRateCells();
+            }
+        });
+
+        nutrient_cells[i].addEventListener("mouseleave", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                hideRateCells();
+            }
+        });
+
+        rate_cells[i].addEventListener("mouseleave", ({relatedTarget}) => {
+            if (!isHoverableElement(relatedTarget)) {
+                hideRateCells();
+            }
+        });
+    }
+
+    for (let i = 0; i < rates.length; ++i) {
+        rates[i].addEventListener("input", ({target}) => {
+            group.rates[i] = target.value;
+            updateNutrientDisplays();
+        });
+    }
+
+    // Insert new row to table
     last_row.parentNode.insertBefore(new_row, last_row);
 
+    // Save food group's servings count for future calculations
     group.servings = servings;
-    group.goal = goal;
-    group.updateChoProFatDisplays = function() {
-        cho.value = `${group.cho_rate * servings.value} g`;
-        pro.value = `${group.pro_rate * servings.value} g`;
-        fat.value = `${group.fat_rate * servings.value} g`;
+
+    // Save updateNutrientDisplays function for future calculations
+    group.updateNutrientDisplays = updateNutrientDisplays;
+
+    // Define hoisted utility functions
+    function updateNutrientDisplays() {
+        for (let i = 0; i < nutrients.length; ++i) {
+            nutrients[i].value = `${rates[i].value * servings.value} g`;
+        }
+    }
+
+    function showRateCells() {
+        for (const cell of rate_cells) {
+            cell.style.setProperty("opacity", "1");
+        }
+    }
+
+    function hideRateCells() {
+        for (const cell of rate_cells) {
+            cell.style.removeProperty("opacity");
+        }
+    }
+
+    function isHoverableElement(element) {
+        return nutrient_cells.includes(element) || rate_cells.includes(element);
     }
 }
 
@@ -164,38 +270,37 @@ for (const group of fill_groups) {
 table.addEventListener("input", () => {
     for (let i = 0; i < fill_groups.length; ++i) {
         const group = fill_groups[i];
-        const goal_name = group.goal_name;
-        const goal_amount = group.goal.value;
-        const rate = group[`${goal_name}_rate`];
+        const goal_index = group.goal_index;
+        const goal_amount = goal_nutrients[goal_index].value;
+        const rate = group.rates[goal_index];
 
         let subtotal = 0;
         for (const group of slider_groups) {
-            subtotal += group.servings.value * group[`${goal_name}_rate`];
+            subtotal += group.servings.value * group.rates[goal_index];
         }
         for (let j = 0; j < i; ++j) {
             const group = fill_groups[j];
-            subtotal += group.servings.value * group[`${goal_name}_rate`];
+            subtotal += group.servings.value * group.rates[goal_index];
         }
         const remaining = goal_amount - subtotal;
 
         group.servings.value = Math.round(remaining / rate);
-        group.updateChoProFatDisplays();
+        group.updateNutrientDisplays();
     }
 
-    let total_cho = 0;
-    let total_pro = 0;
-    let total_fat = 0;
+    const total_nutrients = [0, 0, 0];
     for (const group of slider_groups) {
-        total_cho += group.servings.value * group.cho_rate;
-        total_pro += group.servings.value * group.pro_rate;
-        total_fat += group.servings.value * group.fat_rate;
+        for (let i = 0; i < total_nutrients.length; ++i) {
+            total_nutrients[i] += group.servings.value * group.rates[i];
+        }
     }
     for (const group of fill_groups) {
-        total_cho += group.servings.value * group.cho_rate;
-        total_pro += group.servings.value * group.pro_rate;
-        total_fat += group.servings.value * group.fat_rate;
+        for (let i = 0; i < total_nutrients.length; ++i) {
+            total_nutrients[i] += group.servings.value * group.rates[i];
+        }
     }
-    calculated_cho.value = `${total_cho} g`;
-    calculated_pro.value = `${total_pro} g`;
-    calculated_fat.value = `${total_fat} g`;
+
+    for (let i = 0; i < calculated_nutrients.length; ++i) {
+        calculated_nutrients[i].value = `${total_nutrients[i]} g`
+    }
 });
